4b2ea44b17370b2e098bda4ae6ad67ad931a5ab6
mathis54300 <mathis54300fox@gmail.com>
2024-02-05 13:19:14 +0100

    SQL pour API geoquizz

---
+++ backend/quiz.geoquizz/sql/quiz.geoquizz.schema.sql
@@ -0,0 +1,13 @@
+-- Insertion de données dans la table `partie`
+INSERT INTO `partie` (`id`, `name`, `ville`, `sequence`)
+VALUES
+    ('1', 'Partie 1', 'Ville A', '[{"id": 1, "url": "image1.jpg", "coordonnees": "12,34"}, {"id": 2, "url": "image2.jpg", "coordonnees": "56,78"}]'),
+    ('2', 'Partie 2', 'Ville B', '[{"id": 3, "url": "image3.jpg", "coordonnees": "90,12"}, {"id": 4, "url": "image4.jpg", "coordonnees": "34,56"}]');
+
+-- Insertion de données dans la table `jouer`
+INSERT INTO `jouer` (`id_partie`, `id_user`, `score`, `etat`, `date`)
+VALUES
+    ('1', 'User1', 100, 1, '2024-02-05 10:00:00'),
+    ('1', 'User2', 80, 1, '2024-02-05 10:30:00'),
+    ('2', 'User1', 120, 1, '2024-02-05 11:00:00'),
+    ('2', 'User2', 90, 1, '2024-02-05 11:30:00');
