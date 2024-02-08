ff9f0195b81d3621fb9f4adf44f2758f1907c271
ElianThe <guiffaul2u@etu.univ-lorraine.fr>
2024-02-08 15:27:45 +0100

    développement en cours de la socket. Possibilité d'envoyer des notifications

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import WebSocket from "@/views/WebSocket.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
