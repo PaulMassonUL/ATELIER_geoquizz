71a6d50e770bdbc93ba05346cefc099f2d8de315
Regis <vavasseu2u@etu.univ-lorraine.fr>
2024-02-09 11:11:05 +0100

    ajout du bouton de déconnexion

--- frontend/src/components/ProfilComponent.vue
+++ frontend/src/components/ProfilComponent.vue
@@ -2,126 +2,124 @@
 import axios from 'axios'
 
 export default {
-  data() {
-    return {
-      profil: {
-        id: 1,
-        username: 'Régis'
-      },
-      scores: [
-        {
-          id: 1,
-          score: 100
-        },
-        {
-          id: 2,
-          score: 200
-        },
-        {
-          id: 3,
-          score: 300
-        },
-        {
-          id: 4,
-          score: 400
-        },
-        {
-          id: 5,
-          score: 500
+    data() {
+        return {
+            profil: {
+                id: 1,
+                username: 'Régis'
+            },
+            scores: [
+                {
+                    id: 1,
+                    score: 100
+                },
+                {
+                    id: 2,
+                    score: 200
+                },
+                {
+                    id: 3,
+                    score: 300
+                },
+                {
+                    id: 4,
+                    score: 400
+                },
+                {
+                    id: 5,
+                    score: 500
+                }
+            ],
+            games: [
+                {
+                    id: 1,
+                    game: 'game1'
+                },
+                {
+                    id: 2,
+                    game: 'game2'
+                },
+                {
+                    id: 3,
+                    game: 'game3'
+                },
+                {
+                    id: 4,
+                    game: 'game4'
+                },
+                {
+                    id: 5,
+                    game: 'game5'
+                }
+            ]
         }
-      ],
-      games: [
-        {
-          id: 1,
-          game: 'game1'
+    },
+    methods: {
+        // méthode qui va permettre de retourner les scores et les parties de l'utilisateur connecté
+        fetchProfil() {
+            axios
+                .get('http://localhost:2080/profil')
+                .then((response) => {
+                    this.profil = response.data
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
         },
-        {
-          id: 2,
-          game: 'game2'
+        logout() {
+            localStorage.removeItem('access_token'); // supprimez le token d'accès du local storage
+            this.$router.push('/'); // redirigez l'utilisateur vers la page de connexion
         },
-        {
-          id: 3,
-          game: 'game3'
-        },
-        {
-          id: 4,
-          game: 'game4'
-        },
-        {
-          id: 5,
-          game: 'game5'
-        }
-      ]
     }
-  },
-  methods: {
-    // méthode qui va permettre de retourner les scores et les parties de l'utilisateur connecté
-    fetchProfil() {
-      axios
-        .get('http://localhost:2080/profil')
-        .then((response) => {
-          this.profil = response.data
-        })
-        .catch((error) => {
-          console.error(error)
-        })
-    }
-  }
 }
 </script>
 
 <template>
-  <div class="mt-5">
-    <h1 id="title" class="text-center">Profil</h1>
-    <div id="profil" class="d-flex justify-content-center align-items-center vh-50 mt-3">
-      <div
-        class="card profil-card mb-5 d-flex justify-content-center align-items-center col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4"
-      >
-        <div class="card-body">
-          <h2 class="card-title">{{ profil.username }}</h2>
-        </div>
-      </div>
-    </div>
+    <div class="mt-5">
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <h1 class="profil-title fw-bold mb-5">Profil de {{ profil.username }}</h1>
+                <button class="btn btn-outline-danger" type="button" @click="logout">Se déconnecter</button>
+            </div>
+        </nav>
 
-    <h2 class="fw-bold">Mes Scores</h2>
-    <div id="scores" class="d-flex flex-wrap mb-5">
-      <div
-        v-for="score in scores"
-        :key="score.id"
-        class="card square-card m-3 d-flex align-items-center justify-content-center col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
-      >
-        <div class="card-body d-flex align-items-center justify-content-center">
-          <h2 class="card-title m-0">{{ score.score }}</h2>
+        <h2 class="fw-bold">Mes Scores</h2>
+        <div id="scores" class="d-flex flex-wrap mb-5">
+            <div
+                    v-for="score in scores"
+                    :key="score.id"
+                    class="card square-card m-3 d-flex align-items-center justify-content-center col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
+            >
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h2 class="card-title m-0">{{ score.score }}</h2>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
 
-    <h2 class="fw-bold">Mes Parties</h2>
-    <div id="games" class="d-flex flex-wrap mb-5">
-      <div
-        v-for="game in games"
-        :key="game.id"
-        class="card square-card m-3 d-flex align-items-center justify-content-center col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
-      >
-        <div class="card-body d-flex align-items-center justify-content-center">
-          <h2 class="card-title m-0">{{ game.game }}</h2>
+        <h2 class="fw-bold">Mes Parties</h2>
+        <div id="games" class="d-flex flex-wrap mb-5">
+            <div
+                    v-for="game in games"
+                    :key="game.id"
+                    class="card square-card m-3 d-flex align-items-center justify-content-center col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
+            >
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <h2 class="card-title m-0">{{ game.game }}</h2>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
 .square-card {
-  box-shadow:
-    0 4px 8px 0 rgba(0, 0, 0, 0.2),
-    0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+  0 6px 20px 0 rgba(0, 0, 0, 0.19);
   transition: 0.3s;
 
   &:hover {
-    box-shadow:
-      0 8px 16px 0 rgba(0, 0, 0, 0.2),
-      0 12px 40px 0 rgba(0, 0, 0, 0.19);
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2),
+    0 12px 40px 0 rgba(0, 0, 0, 0.19);
     cursor: pointer;
   }
 }
@@ -132,8 +130,13 @@ export default {
   padding: 20px;
   margin-bottom: 20px;
   border-radius: 5px;
-  box-shadow:
-    0 4px 8px 0 rgba(0, 0, 0, 0.2),
-    0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+  0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.profil-title {
+  font-size: 2em; /* Augmenter la taille de la police */
+  color: #343a40; /* Changer la couleur du texte */
+  text-align: center; /* Aligner le texte à gauche */
 }
 </style>
